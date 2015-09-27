79cfc5b26ca8bec739e455d0dbac56b0d20f009e
Mike king <mikkelking@hotmail.com>
2015-09-27 17:44:35 +1000

    Pass back array now, specify widths as an array

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary:  "say-cheese - plugin for Meteor",
-	version: '1.0.1',
+	version: '1.0.2',
 	name: "mikkelking:say-cheese",
 	git: 'https://github.com/mikkelking/say-cheese'
 });
